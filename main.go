84161bcdde05f42283bf00a83bbf9b844dcfd31d
einge23 <einge823@gmail.com>
2025-05-21 20:18:54 -0400

    update port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +16,12 @@ func main() {
 		})
 	})
 
-	if err := r.Run(":8080"); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := r.Run(":" + port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
